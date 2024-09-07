f3d809620eb4b052b0b55044e8ea0e4e134427e3
FacuCesare <facundo.cesare@outlook.es>
2024-09-07 15:51:19 -0300

    Pausa
    Probando

---
+++ Assets/Scripts/Pausajuego.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GameController : MonoBehaviour
+{
+    // Variable para saber si el juego está pausado o no
+    private bool isPaused = false;
+
+    void Update()
+    {
+        // Detecta la presión de una tecla para pausar el juego (por ejemplo, la tecla 'P')
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            TogglePause();
+        }
+    }
+
+    void TogglePause()
+    {
+        
+        isPaused = !isPaused;
+
+        if (isPaused)
+        {
+           
+            Time.timeScale = 0f;
+        }
+        else
+        {
+           
+            Time.timeScale = 1f;
+        }
+    }
+}
+
