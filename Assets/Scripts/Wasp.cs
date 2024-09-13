a49193e4059322fe2a822d0f57dfc0972b32eeda
el Carlo <elcarlo410@gmail.com>
2024-09-12 21:57:10 -0300

    ataque agregado
    ataque agregado

--- Assets/Scripts/Wasp.cs
+++ Assets/Scripts/Wasp.cs
@@ -4,28 +4,43 @@ using UnityEngine;
 
 public class Wasp : Enemy
 {
-    public Transform player; // Referencia al transform del jugador
-    public float attackRange = 5f; // Rango de ataque
+    //public Transform player; // Referencia al transform del jugador //Lo comento por que no es necesario crear esta línea, ya tenemos de herencia la linea playerPosition y a ella nos debemos referir en el código
+    //public float attackRange = 5f; // Rango de ataque// Lo comento por que no veo donde lo usamos, despues lo chequeamos
+    
 
-    private void Update()
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player") 
+        {           
+            AttackPlayer(); 
+        }
+    }
+
+    
+
+    public void Update()
     {
 
         DetectingPlayer();
 
-
-        /* if (Vector3.Distance(transform.position, player.position) <= range)
+        
+        /*if (Vector3.Distance(transform.position, playerPosition.position) <= range)
         {
             AttackPlayer();
-        }
-        */
+        }*/
+
+        
+        
     }
 
-    private void AttackPlayer()
+    public void AttackPlayer()
     {
-        PlayerHealth playerHealth = player.GetComponent<PlayerHealth>();
+        PlayerHealth playerHealth = playerPosition.GetComponent<PlayerHealth>();
         if (playerHealth != null)
         {
             playerHealth.TakeDamage(dmg);
+            Debug.Log("Te comiste " + dmg);
         }
     }
+    
 }
