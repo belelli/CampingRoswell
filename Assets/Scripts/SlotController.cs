4764b97a890b76d488702502d243a56758ab1021
Nicolas Belelli <nicolas.belelli@davinci.edu.ar>
2024-10-24 10:11:43 -0300

    Letter canvas fix to open only for letter items

--- Assets/Scripts/SlotController.cs
+++ Assets/Scripts/SlotController.cs
@@ -17,28 +17,36 @@ public class SlotController : MonoBehaviour
 
     public void clickSlot(int slotId)
     {
+        
+        
         print("Slock cliqueado :"+slotId);
 
-        //Cuando se toca una carta, se activa el canvas
-        canvas.gameObject.SetActive(true);
 
-        //Path a la carpeta de cartas
-        string readFromFilePath = Application.streamingAssetsPath + "/Letters/" + inventary.slots[slotId].GetComponent<slot>().letterName + ".txt";
-
-
-
-        //Crea lista de strings con todas las lineas del txt
-        List<string> fileLines = File.ReadAllLines(readFromFilePath).ToList();
-
-        // Genera el string a partir de la lista
-        string _textBuildUp = "";
-        foreach (string line in fileLines)
+        string itemType = inventary.slots[slotId].GetComponent<slot>().type;
+        if (itemType == "letter")
         {
-            _textBuildUp += line + "\n";
-        }
+            //Cuando se toca una carta, se activa el canvas
+            canvas.gameObject.SetActive(true);
 
-        //popula el campo Texto de Recall text
-        recallTextObject.GetComponent<TextMeshProUGUI>().text = _textBuildUp;
+            //Path a la carpeta de cartas
+            string readFromFilePath = Application.streamingAssetsPath + "/Letters/" + inventary.slots[slotId].GetComponent<slot>().letterName + ".txt";
+
+
+
+            //Crea lista de strings con todas las lineas del txt
+            List<string> fileLines = File.ReadAllLines(readFromFilePath).ToList();
+
+            // Genera el string a partir de la lista
+            string _textBuildUp = "";
+            foreach (string line in fileLines)
+            {
+                _textBuildUp += line + "\n";
+            }
+
+            //popula el campo Texto de Recall text
+            recallTextObject.GetComponent<TextMeshProUGUI>().text = _textBuildUp;
+        }
+        
 
     }
 }
