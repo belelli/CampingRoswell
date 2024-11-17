49bd9e623970489b8b75b70596fab732fd70e85e
FacuCesare <facundo.cesare@outlook.es>
2024-11-17 20:18:34 -0300

    Sistema Dialogo Terminado

--- Assets/Scripts/Dialogue.cs
+++ Assets/Scripts/Dialogue.cs
@@ -1,14 +1,83 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Dialogue : MonoBehaviour
 {
     private bool isPlayerInRange;
-
+    private bool didDialogueStart;
+    private int lineIndex;
+    private float TypingTime = 0.05f;
+    [SerializeField] private GameObject DialogueMark;
+    [SerializeField, TextArea(4, 6)] private string[] dialogueLines;
+    [SerializeField] private GameObject DialoguePanel;
+    [SerializeField] private TMP_Text dialogueText;
     private void Update()
     {
+        if (isPlayerInRange && Input.GetKeyDown(KeyCode.M))
+        {
+            if (!didDialogueStart)
+            {
+                StartDialogue();
+
+            }
+            else if (dialogueText.text == dialogueLines[lineIndex]) 
+            {
+                NextDialogueLine();
+            
+            }
+            else 
+            {
+                StopAllCoroutines();
+                dialogueText.text = dialogueLines[lineIndex];
+            
+            }
+
+        }
+    }
+
+    private void StartDialogue()
+    {
+        didDialogueStart = true;
+        DialoguePanel.SetActive(true);
+        DialogueMark.SetActive(false);
+        lineIndex = 0;
+        Time.timeScale = 0f;
+        StartCoroutine(ShowLine());
+
+    }
+
+    private void NextDialogueLine()
+    {
+        lineIndex++;
+        if(lineIndex < dialogueLines.Length) 
+        {
+            StartCoroutine(ShowLine());
         
+        }
+        else 
+        {
+            didDialogueStart = false;
+            DialoguePanel.SetActive (false);
+            DialogueMark.SetActive(true);
+            Time.timeScale = 1f;
+        
+        }
+    
+    }
+
+    private IEnumerator ShowLine() 
+    {
+        dialogueText.text = string.Empty;
+
+        foreach (char ch in dialogueLines[lineIndex])
+        {
+            dialogueText.text += ch;
+            yield return new WaitForSecondsRealtime(TypingTime);
+        
+        }
+    
     }
 
     private void OnTriggerEnter(Collider other)
@@ -16,6 +85,7 @@ public class Dialogue : MonoBehaviour
         if (other.gameObject.CompareTag("Player"))
         {
             isPlayerInRange = true;
+            DialogueMark.SetActive(true);
             Debug.Log("Se puede iniciar Dialogo");
 
         }
@@ -27,6 +97,7 @@ public class Dialogue : MonoBehaviour
         if (other.gameObject.CompareTag("Player"))
         {
             isPlayerInRange = false;
+            DialogueMark.SetActive(false);
             Debug.Log("No se puede iniciar Dialogo");
 
         }
