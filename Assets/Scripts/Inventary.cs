870cf4322bdcaf6e492e6691c8b0b8f7f2c8accd
Nicolas Belelli <nicolas.belelli@davinci.edu.ar>
2024-11-23 23:51:58 -0300

    fix build error

--- Assets/Scripts/Inventary.cs
+++ Assets/Scripts/Inventary.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEditor.Rendering;
 using UnityEngine;
-using static UnityEditor.Progress;
+//using static UnityEditor.Progress;
 
 public class Inventary : MonoBehaviour
 {
